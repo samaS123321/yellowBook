88dd250d865cbe98dd41b066817990c5db58a7b1
zhyiphone <544607291@qq.com>
2022-10-20 15:03:42 +0800

    wao12

--- yellowBook/ViewController.swift
+++ yellowBook/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("123")
+        print("1234")
         // Do any additional setup after loading the view.
     }
 
